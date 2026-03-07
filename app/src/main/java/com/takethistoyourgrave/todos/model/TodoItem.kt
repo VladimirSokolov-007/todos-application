7de674608e07d000efc3033fa0f66aa67b4169c0
VladimirSokolov-007 <kirovasvetlana35@gmail.com>
2026-03-07 18:43:24 +0500

    Сделал реализацию TodoItem, JSON-расширений и FileStorage
    - TodoItem data class, TodoItemJson расширение для парсинга JSON                                                   - FileStorage: хранение, добавление, удаление, save/load в файл
    - Самоуничтожение просроченных дел при загрузке

---
+++ app/src/main/java/com/takethistoyourgrave/todos/model/TodoItem.kt
@@ -0,0 +1,16 @@
+package com.takethistoyourgrave.todos.model
+
+import android.graphics.Color
+import java.time.LocalDateTime
+import java.util.UUID
+
+data class TodoItem(
+    val uid: String = UUID.randomUUID().toString(),
+    val text: String,
+    val importance: Importance,
+    val color: Int = Color.WHITE,
+    val deadline: LocalDateTime? = null,
+    val isDone: Boolean = false
+) {
+    companion object
+}
