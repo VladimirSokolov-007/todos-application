473b134c9da0fcc3c5a123f6405c0bedc5279f9f
VladimirSokolov-007 <kirovasvetlana35@gmail.com>
2026-03-10 01:26:56 +0500

    Убрал инициализацию дефолтных тудушек

--- app/src/main/java/com/takethistoyourgrave/todos/MainActivity.kt
+++ app/src/main/java/com/takethistoyourgrave/todos/MainActivity.kt
@@ -4,43 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.lifecycle.lifecycleScope
-import com.takethistoyourgrave.todos.domain.model.Importance
-import com.takethistoyourgrave.todos.domain.model.TodoItem
-import com.takethistoyourgrave.todos.domain.TodoRepository
 import com.takethistoyourgrave.todos.navigation.ToDosNavigation
 import com.takethistoyourgrave.todos.ui.theme.ToDosTheme
-import kotlinx.coroutines.launch
-import org.koin.android.ext.android.inject
-import java.time.LocalDateTime
 
 class MainActivity : ComponentActivity() {
-
-    private val repository: TodoRepository by inject()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-
-        if (repository.getItems().isEmpty()) {
-            lifecycleScope.launch {
-                repository.addItem(
-                    TodoItem(
-                        text = "Купить молоко",
-                        importance = Importance.NORMAL,
-                        isDone = true
-                    )
-                )
-                repository.addItem(
-                    TodoItem(
-                        text = "Сдать лабу",
-                        importance = Importance.HIGH,
-                        deadline = LocalDateTime.now().plusDays(3)
-                    )
-                )
-                repository.addItem(TodoItem(text = "Посмотреть фильм", importance = Importance.LOW))
-            }
-        }
 
         setContent {
             ToDosTheme {
