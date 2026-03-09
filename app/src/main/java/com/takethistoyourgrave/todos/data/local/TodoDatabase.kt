de880e316f9e37945ed20faacec8651490f2d3aa
VladimirSokolov-007 <kirovasvetlana35@gmail.com>
2026-03-10 01:33:00 +0500

    Добавил усложение - миграцию с добавленим цвета

--- app/src/main/java/com/takethistoyourgrave/todos/data/local/TodoDatabase.kt
+++ app/src/main/java/com/takethistoyourgrave/todos/data/local/TodoDatabase.kt
@@ -4,12 +4,19 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 
-@Database(entities = [TodoEntity::class], version = 1)
+@Database(entities = [TodoEntity::class], version = 2)
 abstract class TodoDatabase : RoomDatabase() {
     abstract fun todoDao(): TodoDao
 
     companion object {
+        private val MIGRATION_1_2 = object : Migration(1, 2) {
+            override fun migrate(db: SupportSQLiteDatabase) {
+                db.execSQL("ALTER TABLE todos ADD COLUMN color INTEGER NOT NULL DEFAULT ${android.graphics.Color.WHITE}")
+            }
+        }
 
         fun create(context: Context): TodoDatabase {
             return Room.databaseBuilder(
@@ -17,6 +24,7 @@ abstract class TodoDatabase : RoomDatabase() {
                 TodoDatabase::class.java,
                 "todos.db"
             )
+                .addMigrations(MIGRATION_1_2)
                 .fallbackToDestructiveMigration()
                 .build()
         }
